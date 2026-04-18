ddd3de6c43a0abad996f043a0065f77b66374fc7
Shoaib Khan <shoaib7878687@gmail.com>
2026-04-18 14:14:22 +0500

    updated changes

--- Currency-Counter/src/Currencyconverter.jsx
+++ Currency-Counter/src/Currencyconverter.jsx
@@ -17,7 +17,35 @@ export default function Currencyconverter(){
 
                 <select>
                 <option value="volve">USD</option>
-                
+                <option value="volve">PKR</option>
+                <option value="volve">INR</option>
+                <option value="volve">EUR</option>
+                <option value="volve">GBP</option>
+                <option value="volve">AED</option>
+                <option value="volve">SAR</option>
+                <option value="volve">JPY</option>
+                <option value="volve">CNY</option>
+                <option value="volve">AUD</option>
+                <option value="volve">CAD</option>
+                <option value="volve">CHF</option>
+                <option value="volve">TRY</option>
+                <option value="volve">QAR</option>
+                <option value="volve">KWD</option>
+                <option value="volve">OMR</option>
+                <option value="volve">BHD</option>
+                <option value="volve">MYR</option>
+                <option value="volve">SGD</option>
+                <option value="volve">THB</option>
+                <option value="volve">KRW</option>
+                <option value="volve">RUB</option>
+                <option value="volve">ZAR</option>
+                <option value="volve">BRL</option>
+                <option value="volve">NZD</option>
+                <option value="volve">HKD</option>
+                <option value="volve">SEK</option>
+                <option value="volve">NOk</option>
+                <option value="volve">DKk</option>
+                <option value="volve">EGp</option>
                 </select>
                 </div>
                 </div>

3f004b03a0be1647bc40453fdbb22288a4fc8f3c
Shoaib Khan <shoaib7878687@gmail.com>
2026-04-20 18:51:01 +0500

    updated changes

--- Currency-Counter/src/Currencyconverter.jsx
+++ Currency-Counter/src/Currencyconverter.jsx
@@ -1,6 +1,57 @@
 import logo from './currency.jpg'
 import './Currency.css'
+import { useState,useEffect} from 'react'
+import axios from 'axios'
+
 export default function Currencyconverter(){
+
+    const [amount , setAmount] = useState("0")
+    const [fromCurrency , setFromCurrency] = useState("USD")
+    const [toCurrency , setToCurrency] = useState("PKR")
+    const [convertedAmount , setConvertedAmount] = useState(null)
+    const [exchangeRates , setExchangeRates] = useState({});
+
+    useEffect(() =>{
+        const apiUrl = `https://v6.exchangerate-api.com/v6/a41933d65651c81305353175/latest/${fromCurrency}`
+        axios.get(apiUrl)
+        .then(
+            response =>{
+                setExchangeRates(response.data.conversion_rates)
+            }
+        )
+
+        .catch(error =>{
+            console.log(error)
+        })
+    },[fromCurrency])
+
+    useEffect(() =>{
+        const ConversionRate = exchangeRates[toCurrency]
+        if(ConversionRate){
+            const converted = amount * ConversionRate
+            setConvertedAmount(converted.toFixed(2))
+        }
+    }, [fromCurrency,amount,toCurrency,exchangeRates])
+
+
+
+
+    const handleChange = (e) => {
+        const {name , value } = e.target;
+        switch(name){
+            case 'amount':
+            setAmount(value)
+            break
+
+            case 'fromCurrency':
+                setFromCurrency(value)
+                break
+
+            case 'toCurrency':
+                setToCurrency(value)
+                break
+        }
+    }
     return(
         <div className='App'>
                 <img src={logo} width="60px"   alt="test" />
@@ -8,82 +59,37 @@ export default function Currencyconverter(){
                 <div className='main'>
                     <div className='form'>
                 <form style={{backgroundColor:"white"}}>
-                    <label>Amount :</label>
-                    <input type="number" placeholder='Enter the amount' />
+                    <label >Amount :</label>
+                    <input name='amount' value={amount} type="number" placeholder='Enter the amount' onChange={handleChange}/>
                 </form>
                 </div>
                 <div className='change'>
                     <label>From Currency :</label>
-                <select size="1">
-                <option value="volve">USD</option>
-                <option value="volve">PKR</option>
-                <option value="volve">INR</option>
-                <option value="volve">EUR</option>
-                <option value="volve">GBP</option>
-                <option value="volve">AED</option>
-                <option value="volve">SAR</option>
-                <option value="volve">JPY</option>
-                <option value="volve">CNY</option>
-                <option value="volve">AUD</option>
-                <option value="volve">CAD</option>
-                <option value="volve">CHF</option>
-                <option value="volve">TRY</option>
-                <option value="volve">QAR</option>
-                <option value="volve">KWD</option>
-                <option value="volve">OMR</option>
-                <option value="volve">BHD</option>
-                <option value="volve">MYR</option>
-                <option value="volve">SGD</option>
-                <option value="volve">THB</option>
-                <option value="volve">KRW</option>
-                <option value="volve">RUB</option>
-                <option value="volve">ZAR</option>
-                <option value="volve">BRL</option>
-                <option value="volve">NZD</option>
-                <option value="volve">HKD</option>
-                <option value="volve">SEK</option>
-                <option value="volve">NOk</option>
-                <option value="volve">DKk</option>
-                <option value="volve">EGP</option>
+                <select name='fromCurrency' value={fromCurrency} onChange={handleChange} size="1"> 
+                 {
+                    Object.keys(exchangeRates).map(currency => (
+                        <option key={currency} value={currency}>
+                            {currency}
+                        </option>
+                    ))
+                 }   
                 </select>
                 </div>
 
                  <div className='change'>
                     <label>To Currency :</label>
-                <select size="1">
-                <option value="volve">USD</option>
-                <option value="volve">PKR</option>
-                <option value="volve">INR</option>
-                <option value="volve">EUR</option>
-                <option value="volve">GBP</option>
-                <option value="volve">AED</option>
-                <option value="volve">SAR</option>
-                <option value="volve">JPY</option>
-                <option value="volve">CNY</option>
-                <option value="volve">AUD</option>
-                <option value="volve">CAD</option>
-                <option value="volve">CHF</option>
-                <option value="volve">TRY</option>
-                <option value="volve">QAR</option>
-                <option value="volve">KWD</option>
-                <option value="volve">OMR</option>
-                <option value="volve">BHD</option>
-                <option value="volve">MYR</option>
-                <option value="volve">SGD</option>
-                <option value="volve">THB</option>
-                <option value="volve">KRW</option>
-                <option value="volve">RUB</option>
-                <option value="volve">ZAR</option>
-                <option value="volve">BRL</option>
-                <option value="volve">NZD</option>
-                <option value="volve">HKD</option>
-                <option value="volve">SEK</option>
-                <option value="volve">NOk</option>
-                <option value="volve">DKk</option>
-                <option value="volve">EGP</option>
+                <select name='toCurrency' value={toCurrency} onChange={handleChange} size="1">
+                 {
+                    Object.keys(exchangeRates).map(currency => (
+                        <option key={currency} value={currency}>
+                            {currency}
+                        </option>
+                    ))
+                 }   
                 </select>
                 </div>
                 </div>
+                <h2 className='h2'>Converted amount : {convertedAmount} </h2>
         </div>
     )
 }
